f64b7358616be1804fc85bcc3379919a17c69389
ckher <ckher@LAPTOP-CUUOLBND.dhcp.wustl.edu>
2022-10-26 17:05:19 -0500

    slay

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -13,8 +13,13 @@ public class RecursiveMethods {
 	 */
 	public static double geometricSum(int n) {
 		
-			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+			int power = n;
+			double sum = Math.pow(0.5, power);
+			if (power == 0) {
+				return 0; 
+			} else {
+				return sum + geometricSum(power - 1);
+			}
 		
 	}
 
@@ -29,12 +34,52 @@ public class RecursiveMethods {
 	public static int gcd(int p, int q) {
 		
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
+		int temp = q;
+		if(q==0) {
+			return p;
+		}
+		else {
+			q = p % q;
+			p = temp;
+			return gcd(p,q);
+		}
 		
 	}
 
 	
+	public static int[] garth(int[] array) {
+		int[] reversed = new int[array.length];
+		if (array.length > 0) {
+			int lastIndex = array.length - 1;
+			for (int index = 0; index <= array.length / 2; ++index) {
+				int mirrorIndex = lastIndex - index;
+				// note:
+				// since we read from array and write to reversed
+				// we do not need to use a temp variable
+				reversed[index] = array[mirrorIndex];
+				reversed[mirrorIndex] = array[index];
+			}
+		}
+		return reversed;
+	}
+	
+	public static int[] helper(int[] array, int index, int[] reversed) {
+		
+		if (index == array.length / 2 && array.length % 2 != 0) {
+			reversed[index] = array[index];
+			return reversed;
+		} else  if (index == array.length){
+			return reversed;
+		} else {	
+			int mirrorIndex = (array.length - 1) - index;
+			reversed[index] = array[mirrorIndex];
+			reversed[mirrorIndex] = array[index];
+			return helper(array, ++index, reversed);
+		}
+	}
 
+	
+	
 	/**
 	 * This method uses recursion to create a reverse of the given array
 	 * 
@@ -44,7 +89,9 @@ public class RecursiveMethods {
 	public static int[] toReversed(int[] array) {
 		
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+			int[] reversed = new int[array.length];
+			reversed = helper(array, 0, reversed);
+			return reversed;
 		
 	}
 
@@ -59,8 +106,25 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
-		
+		if (radius < radiusMinimumDrawingThreshold) {
+		}
+		else {
+			StdDraw.circle(xCenter, yCenter, radius);
+			circlesUponCircles(xCenter + radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter + radius, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter - radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter , yCenter -radius, radius/3.0, radiusMinimumDrawingThreshold);
+
+		}
 		// FIXME
 	}
-
+//
+//	public static void main(String args[]) {
+//		int[] array = {1, 2, 3, 4};
+//		int[] reversed = toReversed(array);
+//		for (int i = 0; i < array.length; i++) {
+//			System.out.println(reversed[i]);
+//		}
+//	}
+	
 }
